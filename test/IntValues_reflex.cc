0795d2e94a6d904d5cdbee8dd99686cdd391a265
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2007-11-06 20:07:52 +0000

    refactored into its own files to allow reuse between tests

---
+++ test/IntValues_reflex.cc
@@ -0,0 +1,46 @@
+// -*- C++ -*-
+//
+// Package:     Common
+// Class  :     IntValues_reflex
+// 
+// Implementation:
+//     Hand create the reflex information for some simple tests
+//
+// Original Author:  Chris Jones
+//         Created:  Wed Oct 31 16:23:47 EDT 2007
+// $Id$
+//
+
+// system include files
+#include "Reflex/Builder/ReflexBuilder.h"
+
+// user include files
+#include "DataFormats/Common/test/IntValues.h"
+
+namespace {
+  using namespace ROOT::Reflex;
+  using namespace test_with_reflex;
+  //Need Reflex dicctionaries for the conversion
+  Type type_intvalue = TypeBuilder("IntValue");
+  Type type_intvalue2 = TypeBuilder("IntValue2");
+  
+  void type_intvalue_d() {
+    ClassBuilder("IntValue", typeid(IntValue), sizeof(IntValue), PUBLIC, STRUCT);
+  }
+  void type_intvalue2_d() {
+    ClassBuilder("IntValue2",typeid(IntValue2),sizeof(IntValue2),PUBLIC, STRUCT).
+    AddBase(type_intvalue,BaseOffset<IntValue,IntValue2>::Get(),PUBLIC);
+  }
+  
+  struct Dictionaries {
+    Dictionaries() {
+      type_intvalue_d();
+      type_intvalue2_d();
+    }
+    ~Dictionaries() {
+      type_intvalue.Unload();
+      type_intvalue2.Unload();
+    }
+  };
+  static Dictionaries instance;
+}
