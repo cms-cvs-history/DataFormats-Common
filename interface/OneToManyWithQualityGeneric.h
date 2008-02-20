d785c81cd8d88a3ae3cb7639fb2700b34c0800ff
cmsbuild <>
2008-02-20 08:47:10 +0000

    This commit was manufactured by cvs2git to create tag 'DataFormats- Common-V07-08-00-06'.
    Sprout from master 2007-11-10 05:39:48 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Implement isAvailable() function'
    Cherrypick from master 2008-02-20 08:47:01 UTC Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch> 'added dictionaries for ValueMap<bool>':
        interface/AssociationMap.h
        interface/AssociationVector.h
        interface/MapRefViewTrait.h
        interface/OneToMany.h
        interface/OneToManyWithQuality.h
        interface/OneToManyWithQualityGeneric.h
        interface/OneToOne.h
        interface/OneToOneGeneric.h
        interface/OneToValue.h
        interface/Ptr.h
        interface/PtrVector.h
        interface/PtrVectorBase.h
        interface/Ref.h
        interface/RefBase.h
        interface/RefCore.h
        interface/RefCoreGet.h
        interface/RefProd.h
        interface/RefToBaseProd.h
        interface/RefVector.h
        interface/RefVectorBase.h
        src/PtrVectorBase.cc
        src/RefCore.cc
        src/classes.h
        src/classes_def.xml
        test/RefCore_t.cpp

--- interface/OneToManyWithQualityGeneric.h
+++ interface/OneToManyWithQualityGeneric.h
@@ -1,55 +1,65 @@
-#ifndef DataFormats_Common_OneToMany_h
-#define DataFormats_Common_OneToMany_h
+#ifndef DataFormats_Common_OneToManyWithQualityGeneric_h
+#define DataFormats_Common_OneToManyWithQualityGeneric_h
 #include "DataFormats/Common/interface/AssociationMapHelpers.h"
 #include "DataFormats/Common/interface/Ref.h"
-#include "DataFormats/Common/interface/RefVector.h"
 #include "DataFormats/Common/interface/RefProd.h"
 #include <map>
 #include <vector>
+#include <algorithm>
+#include <boost/lambda/lambda.hpp>
+#include <boost/lambda/bind.hpp>
+#include "DataFormats/Common/interface/MapRefViewTrait.h"
 
 namespace edm {
-  template<typename CKey, typename CVal, typename index = unsigned int>
-  class OneToMany {
+  template<typename CKey, typename CVal, typename Q, typename index = unsigned int,
+    typename KeyRefProd = typename helper::MapRefViewTrait<CKey>::refprod_type, 
+    typename ValRefProd = typename helper::MapRefViewTrait<CVal>::refprod_type, 
+    typename KeyRef = typename helper::MapRefViewTrait<CKey>::ref_type, 
+    typename ValRef = typename helper::MapRefViewTrait<CVal>::ref_type >
+  class OneToManyWithQualityGeneric {
     /// reference to "key" collection
-    typedef edm::RefProd<CKey> KeyRefProd;
+    typedef KeyRefProd keyrefprod_type;
     /// reference to "value" collection
-    typedef edm::RefProd<CVal> ValRefProd;
+    typedef ValRefProd valrefprod_type;    
     /// internal map associated data
-    typedef std::vector<index> map_assoc;
+    typedef std::vector<std::pair<index, Q> > map_assoc;
+
   public:
     /// values reference collection type
-    typedef edm::RefVector<CVal> val_type;
+    typedef std::vector<std::pair<ValRef, Q> > val_type;
     /// insert key type
-    typedef edm::Ref<CKey> key_type;
+    typedef KeyRef key_type;
     /// insert val type
-    typedef edm::Ref<CVal> data_type;
+    typedef std::pair<ValRef, Q> data_type;
     /// index type
     typedef index index_type;
     /// map type
-    typedef std::map<index_type, map_assoc > map_type;
+    typedef std::map<index_type, map_assoc> map_type;
     /// reference set type
-    typedef helpers::KeyVal<KeyRefProd, ValRefProd> ref_type;
+    typedef helpers::KeyVal<keyrefprod_type, valrefprod_type> ref_type;
     /// transient map type
     typedef std::map<const typename CKey::value_type *,
-    		     std::vector<const typename CVal::value_type *> 
+    		     std::vector<std::pair<const typename CVal::value_type *, Q > > 
                     > transient_map_type;
     /// transient key vector
     typedef std::vector<const typename CKey::value_type *> transient_key_vector;
     /// transient val vector
-    typedef std::vector<std::vector<const typename CVal::value_type *> > transient_val_vector;
+    typedef std::vector<std::vector<std::pair<const typename CVal::value_type *, Q > > 
+                       > transient_val_vector;
     /// insert in the map
     static void insert(ref_type & ref, map_type & m,
 			const key_type & k, const data_type & v) {
-      if (k.isNull() || v.isNull())
+      const ValRef & vref = v.first;
+      if (k.isNull() || vref.isNull())
 	throw edm::Exception(edm::errors::InvalidReference)
 	  << "can't insert null references in AssociationMap";
       if (ref.key.isNull()) {
-	ref.key = KeyRefProd(k);
-	ref.val = ValRefProd(v);
+	ref.key = keyrefprod_type(k);
+	ref.val = valrefprod_type(vref);
       }
-      helpers::checkRef(ref.key, k); helpers::checkRef(ref.val, v);
-      index_type ik = index_type(k.key()), iv = index_type(v.key());
-      m[ ik ].push_back(iv);
+      helpers::checkRef(ref.key, k); helpers::checkRef(ref.val, vref);
+      index_type ik = index_type(k.key()), iv = index_type(vref.key());
+      m[ik].push_back(std::make_pair(iv, v.second));
     }
     static void insert(ref_type & ref, map_type & m, const key_type & k, const val_type & v) {
       for(typename val_type::const_iterator i = v.begin(), iEnd = v.end(); i != iEnd; ++i)
@@ -59,13 +69,21 @@ namespace edm {
     static val_type val(const ref_type & ref, const map_assoc & iv) {
       val_type v;
       for(typename map_assoc::const_iterator idx = iv.begin(), idxEnd = iv.end(); idx != idxEnd; ++idx)
-	v.push_back(edm::Ref<CVal>(ref.val, *idx));
+	v.push_back(std::make_pair(ValRef(ref.val, idx->first), idx->second));
       return v;
     }
     /// size of data_type
     static typename map_type::size_type size(const map_assoc & v) { return v.size(); }
     /// sort
-    static void sort(map_type &) { }
+    static void sort(map_type & m) {
+      //      using namespace boost::lambda;
+      for(typename map_type::iterator i = m.begin(), iEnd = m.end(); i != iEnd; ++i) {
+	map_assoc & v = i->second;
+	double std::pair<index, Q>:: *quality = &std::pair<index, Q>::second;
+	std::sort(v.begin(), v.end(),
+		  bind(quality, boost::lambda::_2) < bind(quality, boost::lambda::_1));
+      }
+    }
     /// fill transient map
     static transient_map_type transientMap(const ref_type & ref, const map_type & map) {
       transient_map_type m;
@@ -73,11 +91,11 @@ namespace edm {
       const CVal & cval = * ref.val;
       for(typename map_type::const_iterator i = map.begin(); i != map.end(); ++ i) {
 	const map_assoc & a = i->second;
-	const typename CKey::value_type * k = & ckey[ i->first ];
-	std::vector<const typename CVal::value_type *> v;
+	const typename CKey::value_type * k = & ckey[i->first];
+	std::vector<std::pair<const typename CVal::value_type *, Q> > v;
 	for(typename map_assoc::const_iterator j = a.begin(); j != a.end(); ++j) {
-	  const typename CVal::value_type * val = & cval[ *j ];
-	  v.push_back(val);
+	  const typename CVal::value_type * val = & cval[j->first];
+	  v.push_back(std::make_pair(val, j->second));
 	}
 	m.insert(std::make_pair(k, v));
       }
@@ -97,10 +115,10 @@ namespace edm {
       const CVal & cval = * ref.val;
       for(typename map_type::const_iterator i = map.begin(); i != map.end(); ++ i) {
 	const map_assoc & a = i->second;
-	std::vector<const typename CVal::value_type *> v;
+	std::vector<std::pair<const typename CVal::value_type *, Q> > v;
 	m.push_back(v);
 	for(typename map_assoc::const_iterator j = a.begin(); j != a.end(); ++j)
-	  m.back().push_back(& cval[ *j ]);
+	  m.back().push_back(std::make_pair(& cval[ j->first ], j->second));
       }
       return m;
     }
